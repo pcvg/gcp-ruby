5bda4230b5bc293b65eb45d8bf7ba4aa38ffd808
Ain Tohvri <ain@flashbit.net>
2019-03-28 20:30:13 +0100

    Fix deploy error over invalid piping

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  docker tag gcp-ruby:$VERSION gcp-ruby:latest
+fi
 
 docker push gcp-ruby:$1
