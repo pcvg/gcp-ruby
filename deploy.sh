1f12f87ac0824bd98515b7633e94bfcf82ddb91b
Ain Tohvri <ain@flashbit.net>
2019-03-29 00:30:07 +0100

    Fix tag creation prior deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -ev
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  docker tag savingsutd/gcp-ruby:$VERSION gcp-ruby:latest
+  docker tag savingsutd/gcp-ruby:$VERSION savingsutd/gcp-ruby:latest
 fi
 
 docker push savingsutd/gcp-ruby:$1
